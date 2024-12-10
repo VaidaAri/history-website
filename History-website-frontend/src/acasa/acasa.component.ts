dfee4fb7f565e1eed5656e46eaad3eb4ae8abed9
ariabcd <ariana.vaida@stud.ubbcluj.ro>
2024-12-10 11:39:46 +0200

    feat: configure the `Image` component connection

--- History-website-frontend/src/acasa/acasa.component.ts
+++ History-website-frontend/src/acasa/acasa.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {RouterModule } from '@angular/router';
 import { MeniuComponent } from '../meniu/meniu.component';
+import { ImagineComponent } from '../imagine/imagine.component';
 @Component({
   selector: 'app-acasa',
   standalone: true,
-  imports: [RouterModule, MeniuComponent],
+  imports: [RouterModule, MeniuComponent, ImagineComponent],
   templateUrl: './acasa.component.html',
   styleUrl: './acasa.component.css'
 })
--- History-website-frontend/src/imagine/imagine.component.ts
+++ History-website-frontend/src/imagine/imagine.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-imagine',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './imagine.component.html',
   styleUrl: './imagine.component.css'
 })
-export class ImagineComponent {
+export class ImagineComponent implements OnInit{
 
+  images: any[] = []; 
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchImages();
+}
+
+fetchImages() {
+  this.http.get<any[]>('http://localhost:8080/api/images').subscribe({
+    next: (data) => {
+      this.images = data;
+      console.log('Images fetched:', this.images);
+    },
+    error: (err) => {
+      console.error('Error fetching images:', err);
+    }
+  });
+}
 }
