661fc21b076ad3a4b4bd72c7a6796a27a9729b7d
ariabcd <ariana.vaida@stud.ubbcluj.ro>
2025-03-29 18:54:17 +0200

    feat : update `Istoric` page style

--- History-website-frontend/src/istoric/istoric.component.ts
+++ History-website-frontend/src/istoric/istoric.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MeniuComponent } from '../meniu/meniu.component';
+import { CadranComponent } from "../cadran/cadran.component";
 @Component({
   selector: 'app-istoric',
   standalone: true,
-  imports: [RouterModule, MeniuComponent],
+  imports: [RouterModule, MeniuComponent, CadranComponent],
   templateUrl: './istoric.component.html',
   styleUrl: './istoric.component.css'
 })
