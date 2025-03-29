930a4b73862e0d1192deccb66d7113989ba43c9a
ariabcd <ariana.vaida@stud.ubbcluj.ro>
2025-03-29 19:06:28 +0200

    feat: update `Rezervari ` page style

--- History-website-frontend/src/evenimente/evenimente.component.ts
+++ History-website-frontend/src/evenimente/evenimente.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MeniuComponent } from "../meniu/meniu.component";
 import { CalendarComponent } from '../calendar/calendar.component';
+import { CadranComponent } from "../cadran/cadran.component";
 @Component({
   selector: 'app-evenimente',
   standalone: true,
-  imports: [RouterModule, MeniuComponent, CalendarComponent],
+  imports: [RouterModule, MeniuComponent, CalendarComponent, CadranComponent],
   templateUrl: './evenimente.component.html',
   styleUrl: './evenimente.component.css'
 })
