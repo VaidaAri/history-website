6994bf487664fa7bf00952d452d4c24ae37bc194
ariabcd <ariana.vaida@stud.ubbcluj.ro>
2025-04-04 12:15:56 +0300

    update `Post` structure

--- History-website-frontend/src/postare/postare.component.ts
+++ History-website-frontend/src/postare/postare.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,8 +8,14 @@ import { CommonModule } from '@angular/common';
   templateUrl: './postare.component.html',
   styleUrl: './postare.component.css'
 })
-export class PostareComponent {
+export class PostareComponent implements OnInit {
   @Input() post: any;
+  currentImageIndex: number = 0;
+  
+  ngOnInit() {
+    // Inițializări
+    this.currentImageIndex = 0;
+  }
   
   // Metoda pentru a construi URL-ul pentru imaginea de pe server
   getImageUrl(imagePath: string): string {
@@ -25,5 +31,30 @@ export class PostareComponent {
     
     // Altfel construim URL-ul pentru imaginea de pe server
     return `http://localhost:8080/api/images/uploads/${imagePath}`;
+  }
+  
+  // Navigare în carusel
+  prevImage() {
+    if (this.currentImageIndex > 0) {
+      this.currentImageIndex--;
+    }
+  }
+  
+  nextImage() {
+    if (this.post.images && this.currentImageIndex < this.post.images.length - 1) {
+      this.currentImageIndex++;
+    }
+  }
+  
+  goToImage(index: number) {
+    if (this.post.images && index >= 0 && index < this.post.images.length) {
+      this.currentImageIndex = index;
+    }
+  }
+  
+  // Deschide imaginea în fereastră nouă pentru vizualizare completă
+  openFullImage(image: any) {
+    const url = this.getImageUrl(image.path);
+    window.open(url, '_blank');
   }
 }
